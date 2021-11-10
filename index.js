87660476777976b0abe9bde8632af8b87e778bdc
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-09 19:37:28 -0500

    Created dependencies in index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
